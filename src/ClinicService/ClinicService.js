1f193bb593496f433c0ef95d22d09a977c47e796
Robert Dabin <mangum.carver@gmail.com>
2017-04-28 01:26:43 +0100

    refactor & ensure only results that match full postcode are returned

--- src/ClinicService/ClinicService.js
+++ src/ClinicService/ClinicService.js
@@ -10,22 +10,40 @@ module.exports = class ClinicService {
         this.config = config;
     }
 
-    getAllByOutwardCode(outwardCode) {
+    getAllByOutwardCode(postCode) {
 
-        return this.http.get(this.config.endpoint + "/partial_postcode?partial_postcode=" + outwardCode)
+        return this.http.get(this.config.endpoint + "/partial_postcode?partial_postcode=" + postCode.outwardCode)
             .then((response) => {
 
-                return {
-                    results: _.map(response.result, (result) => {
+                let exactPostCodeResults = this.getExactPostcodeMatches(response.result, postCode.fullcode);
 
-                        return {
-                            organisation_id: result.organisation_id,
-                            name: result.name
-                        }
-                    })
-                };
+                return this.formatResults(exactPostCodeResults);
 
             });
+
+    }
+
+    getExactPostcodeMatches(responseResult, fullcode) {
+
+        let results = _.cloneDeep(responseResult);
+
+        return _.filter(results, (result) => {
+
+            return result.postcode === fullcode;
+
+        });
+    }
+
+    formatResults(clinicData) {
+
+        return {
+            results: _.map(clinicData, (clinic) => {
+                return {
+                    organisation_id: clinic.organisation_id,
+                    name: clinic.name
+                }
+            })
+        };
     }
 
 };
--- src/ClinicService/ClinicService.spec.js
+++ src/ClinicService/ClinicService.spec.js
@@ -8,50 +8,78 @@ const clinicService = new ClinicService(new HttpMock());
 
 describe("ClinicService", () => {
 
+    let mockPostcode = {
+        fullcode: "mockCode",
+        outwardCode: "SW1"
+    };
+
+    clinicService.http.response = {
+        "success":true,
+        "result":[{
+            "postcode":"mockCode",
+            "partial_postcode":"SW1",
+            "organisation_id":"40957",
+            "name":"Dodds Clinic"
+        },{
+            "postcode":"mockCode",
+            "partial_postcode":"SW1",
+            "organisation_id":"40755",
+            "name":"Battersea Clinic"
+        }]
+    };
+
     describe("when asked for clinics in a given outward code", () => {
 
         it("should make correct http request", () => {
 
             let getSpy = sinon.spy(clinicService.http, "get");
 
-            return clinicService.getAllByOutwardCode("mockCode").then(() => {
+            return clinicService.getAllByOutwardCode(mockPostcode).then(() => {
 
-                expect(getSpy.args[0][0]).to.equal("http://mock.endpoint/partial_postcode?partial_postcode=mockCode");
+                expect(getSpy.args[0][0]).to.equal("http://mock.endpoint/partial_postcode?partial_postcode=SW1");
 
             });
 
         });
 
-    });
+        it("should return data in correct format", () => {
 
-    it("should return data in correct format", () => {
+            const expectedResults = {
+                results: [{
+                    organisation_id: "40957",
+                    name: "Dodds Clinic"
+                }, {
+                    organisation_id: "40755",
+                    name: "Battersea Clinic"
+                }]
+            };
 
-        const expectedResults = {
-            results: [{
-                organisation_id: "40957",
-                name: "Dodds Clinic"
-            }, {
-                organisation_id: "40755",
-                name: "Battersea Clinic"
-            }]
-        };
+            return clinicService.getAllByOutwardCode(mockPostcode).then((clinics) => {
 
-        clinicService.http.response = {
-            "success":true,
-            "result":[{
-                "partial_postcode":"SW1",
-                "organisation_id":"40957",
-                "name":"Dodds Clinic"
-            },{
-                "partial_postcode":"SW1",
-                "organisation_id":"40755",
-                "name":"Battersea Clinic"
-            }]
-        };
+                expect(clinics).to.deep.equal(expectedResults);
 
-        return clinicService.getAllByOutwardCode("mockCode").then((clinics) => {
+            });
 
-            expect(clinics).to.deep.equal(expectedResults);
+        });
+
+        describe("when response contains results that do not match full postcode", () => {
+
+            it("should filter these out from the response", () => {
+
+                clinicService.http.response.result.push({
+                    "postcode":"notMockCode",
+                    "partial_postcode":"SW1",
+                    "organisation_id":"40755",
+                    "name":"Battersea Clinic"
+                });
+
+                return clinicService.getAllByOutwardCode(mockPostcode).then((clinics) => {
+
+                    expect(clinics.results.length).to.equal(2);
+
+                });
+
+            });
 
         });
 
